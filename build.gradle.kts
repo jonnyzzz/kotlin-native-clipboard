be0e284876898771641d51aa79d82e34d4f8f19e
Eugene Petrenko <eugene.petrenko@gmail.com>
2019-02-21 21:39:55 +0100

    cinterop for libpng

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,11 +12,24 @@ repositories {
   mavenCentral()
 }
 
+val libpng by configurations.creating
+
+dependencies {
+  libpng(project(path = ":libpng", configuration = "libpng"))
+}
 
 kotlin {
   macosX64 {
+    val main by compilations
+
     compilations.forEach {
       it.kotlinOptions.freeCompilerArgs += "-Xuse-experimental=kotlin.ExperimentalUnsignedTypes"
+    }
+
+    main.cinterops.create("libpng") {
+      packageName = "org.jonnyzzz.png"
+      
+      includeDirs( Callable { File(libpng.singleFile, "include") } )
     }
 
     binaries {
